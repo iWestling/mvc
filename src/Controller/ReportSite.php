af5236ad0582adf0fb32f80bb6994634bf706fbd
Isabel Westling <iswt23@student.bth.se>
2024-04-01 23:43:53 +0200

    First commit

---
+++ src/Controller/ReportSite.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReportSite extends AbstractController
+{
+    #[Route("/lucky", name: "lucky_number")]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        $imageNames = ['lucky1.jpg', 'lucky2.jpg', 'lucky3.jpg'];
+        $randomImage = $imageNames[array_rand($imageNames)];
+
+        $data = [
+            'number' => $number,
+            'image' => $randomImage
+        ];
+
+        return $this->render('lucky_number.html.twig', $data);
+    }
+    
+    #[Route("/home", name: "home")]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+
+    #[Route("/api", name: "api")]
+    public function apiHome(): Response
+    {
+        $routes = [
+            'api_lucky_number' => '/api/lucky',
+            'api_quote' => '/api/quote'
+        ];
+
+        return $this->render('api.html.twig', ['routes' => $routes]);
+    }
+}
---
+++ src/Controller/ReportSiteJson.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReportSiteJson
+{
+    #[Route("/api/lucky", name: "api_lucky_number")]
+    public function jsonNumber(): Response
+    {
+        $number = random_int(0, 100);
+
+        $data = [
+            'lucky-number' => $number,
+            'lucky-message' => 'Hi there!',
+        ];
+
+        // prettyprint
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("/api/quote", name: "api_quote")]
+    public function jsonQuote(): JsonResponse
+    {
+        $quotes = [
+            "Kom igen grabbar, dom har också en dålig målvakt! - Christer Abris",
+            "Då sa jag till domaren på ren svenska, go home! - Christer Abris",
+            "Jag är så grymt besviken på Börje, han är så jävla dålig - Peter Forsberg",
+            "I owe a lot to my parents, especially by mother and my father. - Greg Norman",
+            "They say that nobody is perfect. Then they tell you practice makes perfect. I wish they'd make up their minds. - Wilt Chamberlain",
+            "Det är alldeles för mycket sport inom idrotten. - Percy Nilsson",
+        ];
+
+        $randomIndex = array_rand($quotes);
+        $quote = $quotes[$randomIndex];
+
+        $date = date("Y-m-d");
+        $timestamp = time();
+
+        $data = [
+            'quote' => $quote,
+            'date' => $date,
+            'timestamp' => $timestamp
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+    
+        return $response;
+    }
+}
