74022e7b737df35f7079092414fd6ef405cdfb71
Isabel Westling <iswt23@student.bth.se>
2024-04-07 22:19:14 +0200

    Updated draw route logic, added form with eventlistener for selecting multiple cards to draw

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -40,98 +40,114 @@ class CardGameController extends AbstractController
     #[Route("/card/deck", name: "card_deck", methods: ["GET"])]
     public function showDeck(SessionInterface $session): Response
     {
-        $deck = $session->get('deck', []);
+        // Generate a new deck of cards
+        $deck = DeckOfCards::generateDeck();
     
-        // If deck is empty, generate and store a new one in the session
-        if (empty($deck)) {
-            $deck = DeckOfCards::generateDeck();
-            $session->set('deck', $deck);
-        }
+        // Store the new deck in the session
+        $session->set('deck', $deck);
     
+        // Render the deck template
         return $this->render('card/deck.html.twig', ['deck' => $deck]);
     }
+    
 
-    #[Route("/card/deck/shuffle", name: "card_shuffle_page", methods: ["GET"])]
-    public function shufflePage(SessionInterface $session): Response
+    #[Route("/card/deck/shuffle", name: "card_shuffle", methods: ["GET", "POST"])]
+    public function shuffleDeck(SessionInterface $session): Response
     {
-        $deck = $session->get('deck', []);
+        $deck = DeckOfCards::generateDeck();
 
-        // If deck is empty, generate and store a new one in the session
-        if (empty($deck)) {
-            $deck = DeckOfCards::generateDeck();
-            $session->set('deck', $deck);
-        }
+        shuffle($deck);
+
+        $session->set('deck', $deck);
 
         return $this->render('card/shuffle.html.twig', ['deck' => $deck]);
     }
 
-    #[Route("/card/deck/shuffle", name: "card_shuffle", methods: ["POST"])]
-    public function shuffleDeck(SessionInterface $session): Response
-    {
-        $deck = $session->get('deck', []);
-
-        // Shuffle the deck
-        shuffle($deck);
-
-        // Store the shuffled deck back in the session
-        $session->set('deck', $deck);
-    
-        // Redirect to the shuffle page
-        return $this->redirectToRoute('card_shuffle_page');
-    }
-
+    //For when you only want to draw 1 card:
     #[Route("/card/deck/draw", name: "card_draw")]
     public function drawCard(SessionInterface $session): Response
     {
-        $deck = $session->get('deck', []);
-    
-        if (empty($deck)) {
-            $this->addFlash('warning', 'No more cards left in the deck.');
+        // Check if session data exists
+        if (!$session->has('deck')) {
+            $this->addFlash('warning', 'No cards in deck. Please shuffle the deck.');
             return $this->redirectToRoute('card_deck');
         }
-    
-        $drawnCard = array_shift($deck);
-        $session->set('deck', $deck);
-    
-        $cardHand = new CardHand();
-        $cardHand->add($drawnCard);
-    
-        return $this->render('card/draw.html.twig', [
-            'drawnCards' => $cardHand->getString(),
-            'remainingCards' => count($deck)
-        ]);
-    }
-    
 
-    #[Route("/card/deck/draw/{number}", name: "card_draw_number", methods: ["GET"])]
-    public function drawNumberCards(int $number, SessionInterface $session): Response
-    {
         $deck = $session->get('deck', []);
-    
+
         if (empty($deck)) {
-            $this->addFlash('warning', 'No more cards left in the deck.');
-            return $this->redirectToRoute('card_deck');
+            // Add flash message if the deck is empty
+            $this->addFlash('warning', 'No more cards left in the deck. You have to reset the deck.');
+        } else {
+            // Draw a card from the deck
+            $drawnCard = array_shift($deck);
+            $session->set('deck', $deck);
+
+            $cardHand = new CardHand();
+            $cardHand->add($drawnCard);
         }
-    
-        $cardHand = new CardHand();
-    
-        for ($i = 0; $i < $number; $i++) {
-            if (!empty($deck)) {
-                $drawnCard = array_shift($deck);
-                $cardHand->add($drawnCard);
-            } else {
-                break;
-            }
-        }
-    
-        $session->set('deck', $deck);
-    
-        return $this->render('card/draw_number.html.twig', [
-            'drawnCards' => $cardHand->getString(),
+
+        // Render the draw template with the drawn card (or without if the deck is empty)
+        return $this->render('card/draw.html.twig', [
+            'drawnCards' => isset($cardHand) ? $cardHand->getString() : [],
             'remainingCards' => count($deck)
         ]);
     }
     
+    //For when you select how many cards you want to draw:
+    #[Route("/card/deck/draw", name: "card_draw_post", methods: ["POST"])]
+    public function drawCardsPost(Request $request, SessionInterface $session): Response
+    {
+        // Retrieve the number of cards from the form submission
+        $number = (int)$request->request->get('number');
+    
+        // Redirect to the route for drawing a specified number of cards
+        return $this->redirectToRoute('card_draw_number', ['number' => $number]);
+    }
+    
+    #[Route("/card/deck/draw/{number}", name: "card_draw_number", methods: ["GET", "POST"])]
+    public function drawNumberCards(Request $request, SessionInterface $session, ?int $number = null): Response
+    {
+        // If the route parameter is not provided, try to get it from the request for POST requests
+        if ($request->getMethod() === 'POST' && $number === null) {
+            $number = (int)$request->request->get('number');
+        }
+    
+        // Check if session data exists
+        if (!$session->has('deck')) {
+            $this->addFlash('warning', 'No cards in deck. Please shuffle the deck.');
+            return $this->redirectToRoute('card_deck');
+        }
+
+        $deck = $session->get('deck', []);
+
+        if (empty($deck)) {
+            // Add flash message if the deck is empty
+            $this->addFlash('warning', 'No more cards left in the deck. You have to reset the deck.');
+        } else {
+            $cardHand = new CardHand();
+
+            // Draw the specified number of cards from the deck
+            for ($i = 0; $i < $number; $i++) {
+                if (!empty($deck)) {
+                    $drawnCard = array_shift($deck);
+                    $cardHand->add($drawnCard);
+                } else {
+                    break; // If there are no more cards left in the deck, stop drawing
+                }
+            }
+
+            // Update the deck in the session
+            $session->set('deck', $deck);
+        }
+
+        // Render the draw_number template with the drawn cards (or without if the deck is empty)
+        return $this->render('card/draw_number.html.twig', [
+            'drawnCards' => isset($cardHand) ? $cardHand->getString() : [],
+            'remainingCards' => count($deck)
+        ]);
+    }
+
     
     // #[Route("/game/pig/test/roll", name: "test_roll_dice")]
     // public function testRollDice(): Response
